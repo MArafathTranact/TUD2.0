e5c680218b829596bc22115a1756df21a28db38c
Mohamed Arafath <MArafath@Tranact.com>
2023-01-19 11:15:47 -0500

    Added bew class to hold contract details

---
+++ TUD2.0/Classes/Contracts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUD2._0.Classes
+{
+    public class Contracts
+    {
+        public string contract_id { get; set; }
+        public string contract_name { get; set; }
+        public string text1 { get; set; }
+        public string text2 { get; set; }
+        public string text3 { get; set; }
+    }
+}
