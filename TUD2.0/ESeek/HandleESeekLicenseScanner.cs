434cc72967a1f46f120d8b8d5ff256145fcf664f
Mohamed Arafath <MArafath@Tranact.com>
2023-02-07 21:35:02 -0500

    Implemented handling Gemalto and ESeek scanner executable logic and posted the images to Jpegger API. Also verified the posting and its workflow

--- TUD2.0/ESeek/HandleESeekLicenseScanner.cs
+++ TUD2.0/ESeek/HandleESeekLicenseScanner.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TUD2._0.Classes;
 using TUD2._0.Interface;
@@ -10,9 +14,68 @@ namespace TUD2._0.ESeek
 {
     internal class HandleESeekLicenseScanner : IHandleTUDCommand
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CloseHandle(IntPtr hSnapshot);
+
+        [DllImport("kernel32.dll")]
+        static extern uint WTSGetActiveConsoleSessionId();
+
+        PostImageToJpegger postImage;
+
+        public HandleESeekLicenseScanner()
+        {
+            postImage = new PostImageToJpegger();
+        }
         public void ProcessCommandHandle(Camera camera, TudCommand command)
         {
+            try
+            {
 
+
+
+                var path = ServiceConfiguration.GetFileLocation("ExecutablePath");
+                var executablePath = string.Format("{0}{1}",
+                                            path,
+                                            @"ESeekScanner.exe");
+
+                if (!File.Exists(executablePath))
+                {
+                    Logger.LogWarningWithNoLock($" No Twain Kodak executable available for operation in {path}");
+                }
+                else
+                {
+
+                    var eSeekDocPath = path + @"ESeekImage.jpg";
+                    var commandStringLog = path + @"ESeekScannerLog.txt";
+
+                    var commandString = $"\"{eSeekDocPath}\" \"{commandStringLog}\"";
+
+                    ApplicationLoader.PROCESS_INFORMATION procInfo;
+                    ApplicationLoader.StartProcessAndBypassUAC(executablePath, commandString, out procInfo);
+
+                    //uint winlogonPid = 0;
+                    // obtain the currently active session id; every logged on user in the system has a unique session id
+                    uint dwSessionId = WTSGetActiveConsoleSessionId();
+
+                    var appdone = false;
+                    while (!appdone)
+                    {
+
+                        Process[] processes = Process.GetProcessesByName("ESeekScanner");
+                        if (processes != null && processes.Any())
+                            appdone = false;
+                        else
+                            appdone = true;
+                        Thread.Sleep(1000);
+                    }
+
+                    Task.Factory.StartNew(() => { postImage.LoadImageToJpegger(commandStringLog, eSeekDocPath , "Kodak Doc Scanner ", command); });
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogExceptionWithNoLock($" Exception at HandleWaycomSignaturePad.ProcessCommandHandle : ", ex);
+            }
         }
     }
 }
--- TUD2.0/Gemalto/HandleGemaltoLicenseScanner.cs
+++ TUD2.0/Gemalto/HandleGemaltoLicenseScanner.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TUD2._0.Classes;
 using TUD2._0.Interface;
@@ -10,9 +14,68 @@ namespace TUD2._0.Gemalto
 {
     internal class HandleGemaltoLicenseScanner : IHandleTUDCommand
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CloseHandle(IntPtr hSnapshot);
+
+        [DllImport("kernel32.dll")]
+        static extern uint WTSGetActiveConsoleSessionId();
+
+        PostImageToJpegger postImage;
+
+        public HandleGemaltoLicenseScanner()
+        {
+            postImage = new PostImageToJpegger();
+        }
         public void ProcessCommandHandle(Camera camera, TudCommand command)
         {
+            try
+            {
 
+
+
+                var path = ServiceConfiguration.GetFileLocation("ExecutablePath");
+                var executablePath = string.Format("{0}{1}",
+                                            path,
+                                            @"GemaltoScanner.exe");
+
+                if (!File.Exists(executablePath))
+                {
+                    Logger.LogWarningWithNoLock($" No Gemalto Scanner executable available for operation in {path}");
+                }
+                else
+                {
+
+                    var gemaltodocPath = path + @"GemaltoLicenseImage.jpg";
+                    var commandStringLog = path + @"GemaltoScannerLog.txt";
+
+                    var commandString = $"\"{gemaltodocPath}\" \"{commandStringLog}\"";
+
+                    ApplicationLoader.PROCESS_INFORMATION procInfo;
+                    ApplicationLoader.StartProcessAndBypassUAC(executablePath, commandString, out procInfo);
+
+                    //uint winlogonPid = 0;
+                    // obtain the currently active session id; every logged on user in the system has a unique session id
+                    uint dwSessionId = WTSGetActiveConsoleSessionId();
+
+                    var appdone = false;
+                    while (!appdone)
+                    {
+
+                        Process[] processes = Process.GetProcessesByName("GemaltoScanner");
+                        if (processes != null && processes.Any())
+                            appdone = false;
+                        else
+                            appdone = true;
+                        Thread.Sleep(1000);
+                    }
+
+                    Task.Factory.StartNew(() => { postImage.LoadImageToJpegger(commandStringLog, gemaltodocPath, "Gemalto License Scanner ", command); });
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogExceptionWithNoLock($" Exception at HandleGemaltoLicenseScanner.ProcessCommandHandle : ", ex);
+            }
         }
     }
 }
